37e7bcbb9b415fa703b14d6d373f6f70279d5926
Jan Schwien <jan5366@gmail.com>
2018-12-19 15:13:25 +0100

    work on main window #2

--- temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/window/MainWindow.java
+++ temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/window/MainWindow.java
@@ -1,6 +1,8 @@
 package de.novacyb.temporal.viewer.ui.window;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,6 +12,9 @@ import java.util.ResourceBundle;
  * Created on 19.12.2018.
  */
 public class MainWindow implements Initializable {
+
+    public AnchorPane contentPane;
+    public TextField filterTextField;
 
     /**
      * Main Window Constructor

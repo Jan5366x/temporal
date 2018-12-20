21783c903da8d4b4c92aeca5b9f5620d06facbf3
Jan Schwien <jan5366@gmail.com>
2018-12-20 13:56:42 +0100

    added some place holder code related to ui development

--- temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/control/TimeLine.java
+++ temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/control/TimeLine.java
@@ -1,6 +1,9 @@
 package de.novacyb.temporal.viewer.ui.control;
 
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 
 /**
  * Temporal Time Line Control
@@ -16,6 +19,21 @@ public class TimeLine extends AnchorPane {
      */
     public TimeLine(final TimeLineView view) {
         this.view = view;
+
+        setPrefHeight(50D);
+
+        // TODO test code for ui experiments
+        final var line = new Line(0D,25D,1200D,25D);
+        //widthProperty().addListener((observable, oldValue, newValue) -> line.setEndX(newValue.doubleValue()));
+
+        line.getStrokeDashArray().addAll(15D, 10D);
+        line.setStroke(new Color(0.4D,0.4D,0.4D,1D));
+        getChildren().add(line);
+
+        final var rect1 = new Rectangle(5D + Math.random() * 500,15D,200D,20D);
+        rect1.setFill(new Color(0.4D,0.7D,0.1D,0.95D));
+        getChildren().add(rect1);
+
     }
 
     // TODO implement timeline
--- temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/control/TimeLineView.java
+++ temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/control/TimeLineView.java
@@ -1,6 +1,7 @@
 package de.novacyb.temporal.viewer.ui.control;
 
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 /**
  * Time Line View Control
@@ -8,8 +9,17 @@ import javafx.scene.control.ScrollPane;
  */
 public class TimeLineView extends ScrollPane {
 
+    private final VBox content = new VBox();
     public TimeLineView() {
         setVbarPolicy(ScrollBarPolicy.ALWAYS);
         setHbarPolicy(ScrollBarPolicy.ALWAYS);
+        setContent(content);
+
+        // TODO remove test code
+        content.getChildren().add(new TimeLine(this));
+        content.getChildren().add(new TimeLine(this));
+        content.getChildren().add(new TimeLine(this));
+        content.getChildren().add(new TimeLine(this));
+        content.getChildren().add(new TimeLine(this));
     }
 }

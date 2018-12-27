66e0a23883a2032cf60b9679b0b4cbc691b5fc9b
Jan Schwien <jan5366@gmail.com>
2018-12-27 22:48:00 +0100

    fixed off center time spots

--- temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/control/TimeSpot.java
+++ temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/control/TimeSpot.java
@@ -19,14 +19,14 @@ public class TimeSpot extends AnchorPane implements ITimeScale {
         setTimeStart(timeStart);
         // TODO test code for ui experiments
 
-        rect1 = new Rectangle(0 ,10D,5D,30D);
+        rect1 = new Rectangle(0 ,10D,4D,30D);
         rect1.setFill(new Color(0.2D,0.2D,0.2D,0.95D));
         getChildren().add(rect1);
     }
 
     @Override
     public void updateTimeScale(double timeScale) {
-        setLayoutX(getTimeStart() * timeScale);
+        setLayoutX((getTimeStart() * timeScale) - (rect1.getWidth() / 2));
     }
 
     public int getTimeStart() {

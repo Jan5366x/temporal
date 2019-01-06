458fd258f84b3ca209ed026123df0da321f1093e
Jan Schwien <jan5366@gmail.com>
2019-01-06 13:18:24 +0100

    added console source window base

---
+++ temporal_viewer/src/main/java/de/novacyb/temporal/viewer/ui/window/ConsoleSourceWindow.java
@@ -0,0 +1,27 @@
+package de.novacyb.temporal.viewer.ui.window;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Console Source Window
+ * Created on 06.01.19.
+ */
+public class ConsoleSourceWindow implements Initializable {
+
+    public Button cancelButton;
+    public Button okButton;
+    public TextArea commandTextArea;
+    public ChoiceBox commandModeChoiceBox;
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+
+    }
+
+}
